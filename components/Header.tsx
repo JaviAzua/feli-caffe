070367407eb13a4637eb97d38cb294984ab25a01
Javier Azua <azua.javi@gmail.com>
2023-01-27 22:45:21 -0300

    restore

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,39 +1,33 @@
 import React from "react";
 import { motion } from "framer-motion";
-import Link from "next/link";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <div className="z-20 bg-white sticky top-0 py-6">
-      <div className="font-andada max-w-[90%] mx-auto">
-        <motion.div
-          initial={{
-            opacity: 0,
-            y: -500,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            duration: 1.5,
-          }}
-          className="flex text-lg md:text-2xl items-center"
-        >
-          <div className="flex-1 cursor-pointer hover:font-bold transition-all duration-500">
-            #
-          </div>
-
-          <div className="flex-1 text-center cursor-pointer hover:font-bold transition-all duration-500">
-            <Link href={"/"}>Proyects</Link>
-          </div>
-          <div className="flex-1 text-right cursor-pointer hover:font-bold transition-all duration-500">
-            <Link href={"/about"}>About Me</Link>
-          </div>
-        </motion.div>
-      </div>
+    <div className="font-andada max-w-[90%] z-20 bg-white mx-auto py-6 sticky top-0">
+      <motion.div
+        initial={{
+          opacity: 0,
+          y: -500,
+        }}
+        animate={{
+          opacity: 1,
+          y: 0,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        className="flex text-lg md:text-2xl items-center"
+      >
+        <div className="flex-1 cursor-pointer font-efect">#</div>
+        <div className="flex-1 text-center cursor-pointer font-efect">
+          Proyects
+        </div>
+        <div className="flex-1 text-right cursor-pointer font-efect">
+          About Me
+        </div>
+      </motion.div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Gallery from "@/components/Gallery";
+import Header from "@/components/Header";
 import { client } from "@/lib/client";
 import { PhotosT } from "@/types";
 import Head from "next/head";
@@ -16,6 +17,10 @@ export default function Home({ photos }: Props) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      {/* Header */}
+
+      <Header />
 
       {/* Gallery */}
 
